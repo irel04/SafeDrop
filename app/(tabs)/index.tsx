b4ae334caad199dba91b310f7fc0e9f7e18c3da3
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-06-03 21:59:05 +0800

    feature(navigation): add home and history navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,22 @@
 import ScreenLayout from "@/components/ScreenLayout";
+import { COLORS } from "@/utils/constant";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <ScreenLayout>
-      <Text>Hello</Text>
+      <View style={styles.homeContainer}>
+        <Text>Hello</Text>
+      </View>
     </ScreenLayout>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  homeContainer: {
+    flex: 1,
+    borderColor: COLORS.BRAND[500],
+    borderWidth: 2,
+    padding: 32,
   },
 });
